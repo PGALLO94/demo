b9d12d5d6ec4211f630975ec7342c4ac686922fb
PGALLO94 <pasqualegallo94@gmail.com>
2022-09-15 03:24:32 +0200

    UPDATE 15/09/2022 03:24

--- src/main/java/com/example/demo/Repository/RectangleRepository.java
+++ src/main/java/com/example/demo/Repository/RectangleRepository.java
@@ -1,12 +1,13 @@
 package com.example.demo.Repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import com.example.demo.Model.Rectangle;
 
 import java.util.UUID;
 
 @Repository
-public interface RectangleRepository extends CrudRepository<Rectangle, Long> {
+public interface RectangleRepository extends JpaRepository<Rectangle, Long> {
 
 }
